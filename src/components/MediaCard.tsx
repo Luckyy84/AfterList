25ad7553bc255e4a2f66a76b5dca62b9ccd1779c
Luckyy <Valimareanuclaudiu2@proton.me>
2026-06-18 01:31:23 +0200

    Improve media card hover scale

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -11,8 +11,8 @@ function MediaCard({ item, onSelect }: MediaCardProps) {
   return (
     <motion.article
       className="media-card-wrapper"
-      whileHover={{ y: -6 }}
-      transition={{ type: 'spring', stiffness: 320, damping: 24 }}
+      whileHover={{ y: -8, scale: 1.055 }}
+      transition={{ type: 'spring', stiffness: 360, damping: 22 }}
     >
       <button
         className="media-card"
